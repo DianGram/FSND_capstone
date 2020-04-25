32aebb119e218a04ea3e45c8c8c59988cc466eae
Dianne Gramling <lee@profilestudios.com>
2020-04-24 20:00:21 -0400

    created models and forms

---
+++ forms.py
@@ -0,0 +1,102 @@
+from flask_wtf import Form
+from wtforms import StringField, SelectField, TextAreaField, DateField
+from wtforms.validators import DataRequired, Regexp, Length
+
+
+class TaskForm(Form):
+    title = StringField(
+        'Title', validators=[DataRequired()]
+    )
+
+    details = TextAreaField(
+        'Details'
+    )
+
+    date_needed = DateField(
+        'Date Needed', validators=[DataRequired()]
+    )
+
+
+class VolunteerForm(Form):
+    name = StringField(
+        'Name', validators=[DataRequired()]
+    )
+
+    address = StringField(
+        'Address', validators=[DataRequired()]
+    )
+
+    city = StringField(
+        'city', validators=[DataRequired()]
+    )
+
+    state = SelectField(
+        'State', validators=[DataRequired()],
+        choices=[
+            ('AL', 'AL'),
+            ('AK', 'AK'),
+            ('AR', 'AR'),
+            ('AZ', 'AZ'),
+            ('CA', 'CA'),
+            ('CO', 'CO'),
+            ('CT', 'CT'),
+            ('DC', 'DC'),
+            ('DE', 'DE'),
+            ('FL', 'FL'),
+            ('GA', 'GA'),
+            ('HI', 'HI'),
+            ('IA', 'IA'),
+            ('ID', 'ID'),
+            ('IL', 'IL'),
+            ('IN', 'IN'),
+            ('KS', 'KS'),
+            ('KY', 'KY'),
+            ('LA', 'LA'),
+            ('MA', 'MA'),
+            ('MD', 'MD'),
+            ('ME', 'ME'),
+            ('MI', 'MI'),
+            ('MN', 'MN'),
+            ('MS', 'MS'),
+            ('MO', 'MO'),
+            ('MT', 'MT'),
+            ('NC', 'NC'),
+            ('ND', 'ND'),
+            ('NE', 'NE'),
+            ('NH', 'NH'),
+            ('NJ', 'NJ'),
+            ('NM', 'NM'),
+            ('NV', 'NV'),
+            ('NY', 'NY'),
+            ('OH', 'OH'),
+            ('OK', 'OK'),
+            ('OR', 'OR'),
+            ('PA', 'PA'),
+            ('RI', 'RI'),
+            ('SC', 'SC'),
+            ('SD', 'SD'),
+            ('TN', 'TN'),
+            ('TX', 'TX'),
+            ('UT', 'UT'),
+            ('VA', 'VA'),
+            ('VT', 'VT'),
+            ('WA', 'WA'),
+            ('WV', 'WV'),
+            ('WI', 'WI'),
+            ('WY', 'WY'),
+        ]
+    )
+
+    zip_code = StringField(
+        'Zip Code', validators=[DataRequired(),
+                                Length(min=5, max=10,  message='Enter a valid'
+                                                               ' Zip Code')]
+    )
+
+    phone_number = StringField(
+        'Phone Number', validators=[DataRequired(),
+                                    Regexp('^[0-9]{3}-[0-9]{3}-[0-9]{4}$',
+                                           message='Phone number should be in'
+                                                   ' the format xxx-xxx-xxxx')
+                                    ]
+    )
--- models.py
+++ models.py
@@ -1,1 +1,88 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+import json
 
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, db_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    db.create_all()
+
+    # with app.app_context():
+    #     db.create_all()
+
+
+'''
+Task class
+Contains the task title, details and date needed
+'''
+
+
+class Task(db.Model):
+    __tablename__ = 'task'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    details = db.Column(db.String)
+    date_needed = db.Column(db.Date)
+
+    def __init__(self, title, details, date_needed):
+        self.title = title
+        self.details = details
+        self.date_needed = date_needed
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'details': self.details,
+            'date_needed': self.date_needed}
+
+
+'''
+Volunteer class
+Contains the volunteer's name, address, and phone number
+'''
+
+
+class Actor(db.Model):
+    __tablename__ = 'actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String(2))
+    zip_code = db.Column(db.String(10))
+    phone_number = db.Column(db.String(12))
+
+    def __init__(self, name, address, city, state, zip_code, phone_number):
+        self.name = name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone_number = phone_number
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'city': self.city,
+            'state': self.state,
+            'phone_number': self.phone_number
+        }
